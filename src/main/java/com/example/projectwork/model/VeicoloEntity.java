c824855a772353339e8582790fa6822d960c26c5
EmanueleKumanaku <EmanueleKumanaku@users.noreply.github.com>
2022-03-23 15:07:56 +0100

    Veicolo

---
+++ src/main/java/com/example/projectwork/model/VeicoloEntity.java
@@ -0,0 +1,107 @@
+package com.example.projectwork.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "veicoli")
+public class VeicoloEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String categoria;
+    private String descrizione;
+    private String alimentazione;
+    private String indirizzo;
+    private String gps;
+    private int disponibilita;
+    private String immagine;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getAlimentazione() {
+        return alimentazione;
+    }
+
+    public void setAlimentazione(String alimentazione) {
+        this.alimentazione = alimentazione;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public String getGps() {
+        return gps;
+    }
+
+    public void setGps(String gps) {
+        this.gps = gps;
+    }
+
+    public int getDisponibilita() {
+        return disponibilita;
+    }
+
+    public void setDisponibilita(int disponibilita) {
+        this.disponibilita = disponibilita;
+    }
+
+    public String getImmagine() {
+        return immagine;
+    }
+
+    public void setImmagine(String immagine) {
+        this.immagine = immagine;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Veicolo [alimentazione=" + alimentazione + ", categoria=" + categoria + ", descrizione=" + descrizione
+                + ", disponibilita=" + disponibilita + ", gps=" + gps + ", id=" + id + ", immagine=" + immagine
+                + ", indirizzo=" + indirizzo + ", status=" + status + "]";
+    }
+
+    
+
+}
