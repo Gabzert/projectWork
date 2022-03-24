2253a6ad103f3d761d3d24c0a13ad86b6d30203a
EmanueleKumanaku <EmanueleKumanaku@users.noreply.github.com>
2022-03-24 15:23:54 +0100

    prova descrizione

--- src/main/java/com/example/projectwork/integration/VeicoliREST.java
+++ src/main/java/com/example/projectwork/integration/VeicoliREST.java
@@ -1,12 +1,17 @@
 package com.example.projectwork.integration;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.example.projectwork.model.VeicoloEntity;
 import com.example.projectwork.service.VeicoloService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +28,20 @@ public class VeicoliREST {
     public List<VeicoloEntity> getVeicoli(){
         return service.getVeicoli();
     }
+/*
+	@PostMapping("/desc/{modello}")
+	public List<VeicoloEntity> getByDesc(@PathVariable String modello){
+
+		ObjectMapper mapper = service.getVeicoli();
+		
+
+		List<VeicoloEntity> veicoli = (List<VeicoloEntity>) obj.get("Students");
+		Object[] rentVeic = veicoli
+    	.stream()
+    	.filter(veicolo -> ((Map)veicolo).get("modello").equals(modello))
+    	.toArray();
+
+	}
+	*/
     
 }
