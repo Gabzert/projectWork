ce0052d88230cca14fa1b5be7c02a565a28a4198
EmanueleKumanaku <EmanueleKumanaku@users.noreply.github.com>
2022-03-24 15:07:46 +0100

    .

--- src/main/java/com/example/projectwork/integration/VeicoliREST.java
+++ src/main/java/com/example/projectwork/integration/VeicoliREST.java
@@ -12,46 +12,48 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 @RequestMapping("api/veicoli")
 public class VeicoliREST {
 
-    @Autowired
-    VeicoloService service;
+	@Autowired
+	VeicoloService service;
 
-    @GetMapping("categoria/{categoria}")
-	public List<VeicoloEntity> getByCategoria(@PathVariable("categoria") String categoria){
-		
+	@GetMapping("categoria/{categoria}")
+	public List<VeicoloEntity> getByCategoria(@PathVariable String categoria) {
+
 		return service.getVeicoloByCategoria(categoria);
-			}
+	}
 
-            @GetMapping("descrizione/{descrizione}")
-	public List<VeicoloEntity> getByDescrizione(@PathVariable("descrizione") String descrizione){
-		
-		return  service.getVeicolocoloByDescrizione(descrizione);
-		
+	@GetMapping("descrizione/{descrizione}")
+	public List<VeicoloEntity> getByDescrizione(@PathVariable String descrizione) {
+
+		return service.getVeicolocoloByDescrizione(descrizione);
+
 	}
-    @GetMapping("indirizzo/{indirizzo}")
-	public List<VeicoloEntity> getByIndirizzo(@PathVariable("indirizzo") String indirizzo){
-		
+
+	@GetMapping("indirizzo/{indirizzo}")
+	public List<VeicoloEntity> getByIndirizzo(@PathVariable String indirizzo) {
+
 		return service.getVeicoloByIndirizzo(indirizzo);
-		
+
 	}
+
 	@GetMapping("alimentazione/{alimentazione}")
-	public List<VeicoloEntity> getByAlimentazione(@PathVariable("alimentazione") String alimentazione){
-		
+	public List<VeicoloEntity> getByAlimentazione(@PathVariable String alimentazione) {
+
 		return service.getVeicoloByAlimentazione(alimentazione);
-		
+
 	}
+
 	@GetMapping("/{min}/{max}")
-	public List<VeicoloEntity> getPrezzoRange(@PathVariable("min")double min, @PathVariable("max")double max){
+	public List<VeicoloEntity> getPrezzoRange(@PathVariable double min, @PathVariable double max) {
 		return service.getRangePrezzo(min, max);
 	}
+
 	@GetMapping("/{id}")
 	public VeicoloEntity getVeicolo(@PathVariable("id") int id) {
-		
+
 		return service.getVeicolo(id);
 	}
 
--- src/test/java/com/example/projectwork/ProjectworkApplicationTests.java
+++ src/test/java/com/example/projectwork/ProjectworkApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.projectwork;
 
+import java.util.List;
+
+import com.example.projectwork.model.VeicoloEntity;
+import com.example.projectwork.repository.VeicoliDB;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class ProjectworkApplicationTests {
 
+	@Autowired
+	VeicoliDB vecDB;
+
 	@Test
-	void contextLoads() {
+	@Transactional
+	void findCar() {
+
+		List<VeicoloEntity> veicoli = vecDB.findByDescrizione("modello");
+
+		for(VeicoloEntity v : veicoli){
+			System.out.println("--------------");
+			System.out.println(v);
+			System.out.println("--------------");
+		}
 	}
+
+
 
 }
