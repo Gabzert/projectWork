25d5676c26a0d7de96d33ef598d84a5c8b9baf19
EmanueleKumanaku <EmanueleKumanaku@users.noreply.github.com>
2022-03-25 18:10:00 +0100

    .

--- src/main/java/com/example/projectwork/integration/VeicoloREST.java
+++ src/main/java/com/example/projectwork/integration/VeicoloREST.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/veicoli")
-public class VeicoliREST {
+public class VeicoloREST {
 
 	@Autowired
 	VeicoloService service;
