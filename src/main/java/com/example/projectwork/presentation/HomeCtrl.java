124acf9437cd3b4a39a4528a35ec67106bef8e50
EmanueleKumanaku <EmanueleKumanaku@users.noreply.github.com>
2022-03-29 11:42:15 +0200

    gestione rotte

--- src/main/java/com/example/projectwork/presentation/HomeCtrl.java
+++ src/main/java/com/example/projectwork/presentation/HomeCtrl.java
@@ -12,9 +12,46 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @RequestMapping("/")
 @SessionAttributes("utente")
 public class HomeCtrl {
+
     @GetMapping
     public String homeCliente(@ModelAttribute UtenteEntity utente) {
         return "homepage";
     }
+
+    @GetMapping("/dispo")
+    public String disponibilita(@ModelAttribute UtenteEntity utente) {
+        return "disponibilita";
+    }
+
+    @GetMapping("/admin")
+    public String admin(@ModelAttribute UtenteEntity utente) {
+        return "admin";
+    }
+
+    @GetMapping("/veicolo")
+    public String veicoloK(@ModelAttribute UtenteEntity utente) {
+        return "veicoloK";
+    }
+
+    @GetMapping("/utente")
+    public String utente(@ModelAttribute UtenteEntity utente) {
+        return "utente";
+    }
+
+    @GetMapping("/pannello")
+    public String pannello(@ModelAttribute UtenteEntity utente) {
+        return "pannello";
+    }
+
+    @GetMapping("/index")
+    public String index(@ModelAttribute UtenteEntity utente) {
+        return "index";
+    }
+
+    /*
+    @GetMapping
+    public String homeCliente(@ModelAttribute UtenteEntity utente) {
+        return "homepage";
+    }*/
     
 }
