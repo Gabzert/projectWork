9a99a7a78cd5f632fb5647c1639e0b7270c69917
EmanueleKumanaku <EmanueleKumanaku@users.noreply.github.com>
2022-03-30 15:09:23 +0200

    modify modal

--- src/main/resources/static/js/modifyVeicolo.js
+++ src/main/resources/static/js/modifyVeicolo.js
@@ -1,21 +1,28 @@
 
-$('document').ready(function(){
 
-    $('#editButton').on('click',function(event){
+async function modifyVeicolo(id){
+    let response = await fetch('http://localhost:8080/api/veicoli/veicolo/' + id);
+    let veicolo1 = await response.json();
 
-        var href = $(this).attr('href');
+    const context = {
+        'veicolo': veicolo1
+    };
 
-        $.get(href, function(veicolo) {
-            $('#id').val(veicolo.id);
-            $('#marca').val(veicolo.marca);
-            $('#modello').val(veicolo.modello);
-            $('#prezzo').val(veicolo.prezzo);
-            $('#immagine').val(veicolo.immagine);
-            $('#descrizione').val(veicolo.descrizione);
-        });
+    handle1("template-swiper1", "ouput-div1", context);
 
-        $('update').modal();
-    });
+    $("#update").modal('show');
 
+}
 
-});
+function handle1(idTemplate, idOutput, context) {			
+			
+    let templateScript = document.getElementById(idTemplate).innerHTML;
+
+    // handlebar
+    let template = Handlebars.compile(templateScript);
+    let htmlCompilato = template(context);
+
+    let output = document.getElementById(idOutput);
+    output.innerHTML = htmlCompilato;
+    
+}
