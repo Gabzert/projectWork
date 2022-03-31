7bd9e4d58e5d173819e6a790333afc56f8e80277
Gabzert <96411750+Gabzert@users.noreply.github.com>
2022-03-31 09:18:39 +0200

    tentativo calendario

--- src/main/resources/static/js/singleCarJs.js
+++ src/main/resources/static/js/singleCarJs.js
@@ -1,11 +1,11 @@
-let veicolo_id =null;
+let veicolo_id = null;
 
-async function getVeicoloById(id){
+async function getVeicoloById(id) {
 
     let response = await fetch('http://localhost:8080/api/veicoli/veicolo/' + id);
     let veicolo = await response.json();
 
-    veicolo_id= id;
+    veicolo_id = id;
 
     const context = {
         'veico': veicolo
@@ -15,30 +15,40 @@ async function getVeicoloById(id){
 }
 
 
-function prenota(event){
+function prenota(event) {
+
     fetch("http://localhost:8080/api/utenti/utente")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (json) {
-       utente_id = json.id;
+        .then(function (response) {
+            if (!response.ok) {
+                window.location.replace("http://localhost:8080/login")
+            }else{
+            return response.json();
+            }
+        })
+        .then(function (json) {
+            utente_id = json.id;
+            data_selezionata = document.getElementById("data").innerText;
+            if (data_selezionata == "") {
+                alert("Selezionare data della prenotazione")
+            } else {
+                fetch("http://localhost:8080/api/prenotazioni/addprenotazione", {
+                    method: "POST",
+                    redirect: "follow",
+                    body: JSON.stringify(
+                        veicolo_id,
+                        utente_id,
+                        data_selezionata
+                    )
+                }).then(res => {
+                    console.log("Request complete! response:", res);
+                });
+            }
+        })
+ }
 
-        fetch("http://localhost:8080/api/prenotazioni/addprenotazione",{
-        method: "POST",
-        body: JSON.stringify(
-            veicolo_id,
-            utente_id,
-            data_selezionata
-        )
-        }).then(res => {
-        console.log("Request complete! response:", res);
-        });
-    })
 
-}
+function handle(idTemplate, idOutput, context) {
 
-function handle(idTemplate, idOutput, context) {			
-			
     let templateScript = document.getElementById(idTemplate).innerHTML;
 
     // handlebar
@@ -48,21 +58,20 @@ function handle(idTemplate, idOutput, context) {
     let output = document.getElementById(idOutput);
     output.innerHTML = htmlCompilato;
 
-    let btn_prenota =document.getElementById("bottone_prenota");
-    btn_prenota.addEventListener("click",prenota);
-
-    
+    let btn_prenota = document.getElementById("bottone_prenota");
+    btn_prenota.addEventListener("click",prenota)
 }
 
+
 window.addEventListener(
-	'DOMContentLoaded',
-	function (event) {
-        
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new fullcalendar.Calendar(calendarEl, {initialView: 'dayGridMonth'});
-        calendar.render();
-	
-		
-	});
+    'DOMContentLoaded',
+    function (event) {
+
+        const input = document.getElementById('data');
+        const datepicker = new TheDatepicker.Datepicker(input);
+        datepicker.render();
+
+
+    });
 
 
